f225608642d717e71dcc3cb31cf746ebbeaac462
dennysh <dennysh437@gmail.com>
2024-06-13 21:31:20 +0900

    a
    a

--- next.config.js
+++ next.config.js
@@ -3,8 +3,9 @@ module.exports = {
   images: {
     unoptimized: true,
   },
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://Leegunhee_test_leesy.ac.kr/"
-      : "",
+  // 이건 github에서 호스팅할때 쓰는거라 문제 생긴듯
+  // assetPrefix:
+  //   process.env.NODE_ENV === "production"
+  //     ? "https://Leegunhee_test_leesy.ac.kr/"
+  //     : "",
 };
